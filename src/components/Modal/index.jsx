998ecccb87b731a27607d08299aa0d886c25a712
lory <hueling.yen@gmail.com>
2019-02-11 23:20:46 +0800

    update modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,53 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import cx from "classnames";
 import "./style.sass";
 
-const modalRoot = document.getElementById('modal');
+const modalRoot = document.getElementById("modal");
 
 class Modal extends Component {
-    constructor(props) {
-        super(props);
-        this.el = document.createElement('div');
-        this.state = {
+  constructor(props) {
+    super(props);
+    // this.el = document.createElement("div");
+    this.state = {};
+  }
 
-        }
+  static getDerivedStateFromProps(props, state) {
+    if (props.isHidden !== state.prevIsHidden) {
+      return {
+        isHidden: props.isHidden,
+        prevIsHidden: props.isHidden
+      };
     }
+    return null;
+  }
 
-    static getDerivedStateFromProps(props, state) {
-        if (props.isHidden !== state.prevIsHidden) {
-            return {
-                isHidden: props.isHidden,
-                prevIsHidden: props.isHidden
-            }
-        }
-        return null;
-    }
+  //   componentDidMount() {
+  //     modalRoot.appendChild(this.el);
+  //   }
 
-    componentDidMount() {
-        modalRoot.appendChild(this.el);
-    }
+  //   componentWillUnmount() {
+  //     modalRoot.removeChild(this.el);
+  //   }
 
-    componentWillUnmount() {
-        modalRoot.removeChild(this.el);
-    }
+  onButtonClick = () => {
+    this.setState({
+      hidden: !this.state.isHidden
+    });
+    this.props.onClose();
+  };
 
-    onButtonClick = () => {
-        this.setState({
-            hidden: !this.state.isHidden
-        });
-        this.props.onClose();
-    }
-
-    render() {
-        return ReactDOM.createPortal(
-            <div className={cx("modal", { hidden: this.state.isHidden })}>
-                <button onClick={this.props.onClose}>X</button>
-                {this.props.children}
-            </div>,
-            this.el
-        );
-    }
+  render() {
+    return ReactDOM.createPortal(
+      <div className={cx("modal", { hidden: this.state.isHidden })}>
+        <button onClick={this.props.onClose}>X</button>
+        {this.props.children}
+      </div>,
+      //this.el
+      document.getElementById("modal")
+    );
+  }
 }
 
 export default Modal;
